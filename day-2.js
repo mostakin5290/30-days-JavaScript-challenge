21ae8da8312c78dc0d843736ce1100f0c4363fd1
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2024-07-14 10:46:03 +0530

    Create day-2.js

---
+++ day-2.js
@@ -0,0 +1,57 @@
+//⁡⁣⁢⁣ Activity 1:Arithmetic Operations⁡
+let num1=30;
+let num2=15;
+// Task 1:
+let sum=num1+num2;
+console.log(sum);
+// Task 2:
+let subtract=num1-num2;
+console.log(subtract);
+// Task 3:
+let multiply=num1*num2;
+console.log(multiply);
+// Task 4:
+let divide=num1/num2;
+console.log(divide);
+// Task 5:
+let remainder=num1%num2;
+console.log(remainder);
+
+// ⁡⁣⁢⁣Activity 2: Assignment Operator⁡
+let a=10;
+// Task 6:
+a+=5;
+console.log(a);
+// Task 7:
+a-=4;
+console.log(a);
+
+// ⁡⁣⁢⁣Activity 3: Comparison Operator⁡
+let b=15;
+let c=18;
+// Task 8:
+console.log(b<c);
+console.log(b>c);
+// Task 9:
+console.log(b<=c);
+console.log(b>=c);
+// Task 10:
+console.log(b==c);
+console.log(b===c);
+
+// ⁡⁣⁢⁣Activity 4: logical Operators⁡
+let d=10
+let e=11;
+let f=12;
+// Task 11: 
+console.log(d<e&&e<f)
+// Task 12:   
+console.log(d>e||e<f)
+// Task 13:   
+console.log(!d)
+
+// ⁡⁣⁢⁣Activity 5: Ternary Operator⁡
+// Task 14:
+let c_Num=-5;
+let ans=(c_Num>=0)?"positive":"negative";
+console.log(ans)
