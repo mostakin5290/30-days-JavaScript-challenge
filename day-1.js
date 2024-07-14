3854434410a9a756f6e7cb2304b10e956d6f31fb
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2024-07-14 10:36:19 +0530

    day-01

---
+++ day-1.js
@@ -0,0 +1,37 @@
+// Activity 1:Variable Declaration
+// Task 1:
+var num=10;
+console.log(num);
+// Task 2:
+let name='Mostakin'
+console.log(name);
+
+// Activity 2:Declaration
+// Task 3:
+const flag=true;
+console.log(flag);
+
+// Activity 3: Data Type
+// Task 4:
+let n=10;
+let batch='cse';
+let temp=false;
+let arr=[1,2,3,4,5];
+let obj={};
+
+console.log(typeof(n))
+console.log(typeof(batch))
+console.log(typeof(temp))
+console.log(typeof(obj))
+console.log(typeof(arr))
+
+
+// Activity 4: Reassigning variable 
+// Task 5:
+let myNumber = 42;
+console.log("Initial value:", myNumber);
+
+myNumber = 8;
+console.log("New value:", myNumber);
+
+
